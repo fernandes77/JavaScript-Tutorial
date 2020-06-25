1d0617f151e0e0369c51b733fbf32b9967148831
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-06-25 12:19:07 -0300

    finished first steps section

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let myName = "Rodrigo Fernandes";
 let iAmAlive = true;
 let myNameCharacters = [7, 7, 2, 5, 9];
 let myInfo = {name: myName, age: myAge, sex: "male"};
+
+let lotsOfDecimal = 1.766584958675746364;
+let twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+let myNumber = "256";
+
+let browserType = "chrome";
+let os = "winDOWS";
+
+let myCountries = "Brasil, Portugal, Angola";
+let myCities = ["Porto", "Lisboa", "Braga"];
+let myArray = ["USA", "England", "Spain"];
