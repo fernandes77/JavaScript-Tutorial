74a50c11246e3d31aaf759260b4c6a0937aad068
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-06-28 13:31:08 -0300

    added prototype section

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,30 +1,61 @@
 const person = {
-  name: ['Bob', 'Smith'],
+  name: ["Bob", "Smith"],
   age: 32,
-  gender: 'male',
-  interests: ['music', 'skiing'],
-  bio: function() {
-    alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+  gender: "male",
+  interests: ["music", "skiing"],
+  bio: function () {
+    alert(
+      this.name[0] +
+        " " +
+        this.name[1] +
+        " is " +
+        this.age +
+        " years old. He likes " +
+        this.interests[0] +
+        " and " +
+        this.interests[1] +
+        "."
+    );
   },
-  greeting: function() {
-    alert('Hi! I\'m ' + this.name[0] + '.');
-  }
+  greeting: function () {
+    alert("Hi! I'm " + this.name[0] + ".");
+  },
 };
 
 person["eyes"] = "hazel";
 
 function Person(first, last, age, gender, interests) {
   this.name = {
-     first : first,
-     last : last
+    first: first,
+    last: last,
   };
   this.age = age;
   this.gender = gender;
   this.interests = interests;
-  this.bio = function() {
-    alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+  this.bio = function () {
+    alert(
+      this.name.first +
+        " " +
+        this.name.last +
+        " is " +
+        this.age +
+        " years old. He likes " +
+        this.interests[0] +
+        " and " +
+        this.interests[1] +
+        "."
+    );
   };
-  this.greeting = function() {
-    alert('Hi! I\'m ' + this.name.first + '.');
+  this.greeting = function () {
+    alert("Hi! I'm " + this.name.first + ".");
   };
 }
+
+let person1 = new Person("Rodrigo", "Fernandes", 22, "male", [
+  "football",
+  "coding",
+]);
+let person3 = new person1.constructor("Karen", "Stephenson", 26, "female", [
+  "playing drums",
+  "mountain climbing",
+]);
